1bb9950af33ce9026377cde97c958f3091f9a3f5
Talha Tariq <talhatariq24603@gmail.com>
2025-09-18 15:58:51 +0500

    first commit

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,5 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Listening on ${port}`));
+
+export default app;
